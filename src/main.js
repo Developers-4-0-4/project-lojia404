d9f096acd9614d8d39bbae5024e0c71eb79988b7
JosueRufino <josuerufino210@gmail.com>
2026-01-08 10:54:54 +0100

    Projeto Vue criado e configurando

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bootstrap JS
+import "bootstrap";
+
+createApp(App).mount("#app");
